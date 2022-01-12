9e5aa15e47256acc14de84ce13ff7d04c01c4b4b
Edwin <espinozaloaiza96@gmail.com>
2022-01-12 16:29:25 -0500

    proyecto

---
+++ Desarrollo/GP4/Sprint 2/Spice-master/Spice/Models/MenuItem.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Spice.Models
+{
+    public class MenuItem
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "El campo es requerido")]
+        [Display(Name = "Plato")]
+        public string Name { get; set; }
+
+        [Display(Name = "Descripcion")]
+        public string Description { get; set; }
+
+        public string Spicyness { get; set; }
+        public enum ESpicy { NA=0,NotSpicy=1, Spicy=2, VerySpicy=3}
+
+        [Display(Name = "Imagen")]
+        public string Image { get; set; }
+
+        [Display(Name="Categoria")]
+        public int CategoryId { get; set; }
+
+        [ForeignKey("CategoryId")]
+        public virtual Category Category { get; set; }
+
+        [Display(Name = "Sub categoria")]
+        public int SubCategoryId { get; set; }
+
+        [ForeignKey("SubCategoryId")]
+        public virtual SubCategory SubCategory { get; set; }
+
+
+        [Display(Name = "Precio")]
+        public double Price { get; set; }
+
+
+    }
+}
---
+++ Desarrollo/GP4/Sprint 2/Spice-master/Spice/Models/SubCategory.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Spice.Models
+{
+    public class SubCategory
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Display(Name = "Nombre")]
+        [Required(ErrorMessage ="El campo es requerido")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "El campo es requerido")]
+        [Display(Name ="Categoria")]
+        public int CategoryId { get; set; }
+
+        [ForeignKey("CategoryId")]
+        public virtual Category Category { get; set; }
+
+    }
+}
